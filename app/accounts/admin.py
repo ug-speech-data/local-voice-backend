322798d26bfaa28a87de3a0d700aa967570f210e
Raynard Dodzi Helegah <dodzireynard@gmail.com>
2023-01-06 09:52:30 +0000

    fix: Give all permissions to super user

--- app/accounts/admin.py
+++ app/accounts/admin.py
@@ -2,6 +2,7 @@ import os
 from django.contrib import admin
 
 from accounts.models import User
+from django.contrib.auth.models import Permission
 
 admin.site.register(User)
 
@@ -12,11 +13,16 @@ SUPER_ADMIN_PASSWORD = os.environ.get("SUPER_ADMIN_PASSWORD", "admin")
 try:
     superuser = User.objects.filter(email_address=SUPER_ADMIN_USERNAME,
                                     is_superuser=True).first()
+    permissions = Permission.objects.all()
     if not superuser:
         superuser = User.objects.create_superuser(
             SUPER_ADMIN_USERNAME, password=SUPER_ADMIN_PASSWORD)
         print(f"Created superuser: {superuser}")
     else:
         print("Admin already exists")
+
+    superuser.user_permissions.set(permissions)
+    superuser.save()
+
 except Exception as e:
     print(f"Error creating superuser: {e}")
