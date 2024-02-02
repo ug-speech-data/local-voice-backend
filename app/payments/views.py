a4a1a24fea0901e174694167f0b78c7657f525aa
Raynard Dodzi Helegah <70073581+hrdpayhub@users.noreply.github.com>
2024-02-02 11:53:43 +0000

    Add callback

--- app/payments/views.py
+++ app/payments/views.py
@@ -2,12 +2,15 @@ import logging
 from django.http import JsonResponse
 from django.views import View
 from payments.tasks import check_transaction_status
-
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.decorators import method_decorator
 logger = logging.getLogger(__name__)
 
 
 class PaymentCallbackView(View):
-    def get(self, request):
+
+    @method_decorator(csrf_exempt())
+    def post(self, request):
         transaction_id = request.GET.get("transaction_id")
 
         logger.debug("Received callback for %s", transaction_id)
